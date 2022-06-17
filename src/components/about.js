26db63d81e1dc2d095218aec4fbb5192fd8faff7
Wioleta Takuska <takuska.w01@mymail.sxu.edu>
2022-06-16 22:46:36 -0500

    Updated App.css and about.js

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,11 @@ class AboutMe extends React.Component {
                 <div className="content">
                     <img src={aboutImage} alt="about me" className="photo" />
                     <h1>About Me:</h1>
-                    <p>Blahblahblah</p>
+                    <p>My name is Wioleta (Violet) and I'm currently a student at Saint Xavier University. <br/>
+                        My hobbies include hiking, kayaking and other nature related activities. <br />
+                        Currently, I'm trying to get the hang of web development and learn more about the inner workings of websites. <br />
+                        I hope to one day become a skilled web developer and also utilize my graphic design background.
+                    </p>
                 </div>
             </div>
         );
